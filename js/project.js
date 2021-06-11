32e45bc0db270311ac4177838e97781c761df6e1
Mrigank Pawagi <mrigankpawagi@gmail.com>
2021-06-11 17:29:36 +0530

    11 June

--- js/project.js
+++ js/project.js
@@ -7,7 +7,10 @@ const citations = ["Jory MacKay in RescueTime:blog - <a href='https://blog.rescu
     "The survey can be found at <a href='http://bit.ly/3cuy0Au'>bit.ly/3cuy0Au</a> and the raw data is available at <a href='https://bit.ly/2OcJTCs'>bit.ly/2OcJTCs</a>.",
     "This is also suggested in a study by Rosekind, Mark R. PhD; Gregory, Kevin B. BS; Mallis, Melissa M. PhD; Brandt, Summer L. MA; Seal, Brian PhD; Lerner, Debra PhD - <a href='https://journals.lww.com/joem/Abstract/2010/01000/The_Cost_of_Poor_Sleep__Workplace_Productivity.13.aspx?utm_source=zapier.com&utm_medium=referral&utm_campaign=zapier'>The Cost of Poor Sleep: Workplace Productivity Loss and Associated Costs</a>.",
     "The smallest unit of time processed by the algorithm is currently 1 minute.",
-    "\\(1 \\text{ day} =\\) \\( 24 \\text{ hours} =\\) \\( 24 \\times 60 \\text{ minutes} =\\) \\( 1440 \\text{ minutes}\\)."
+    "\\(1 \\text{ day} =\\) \\( 24 \\text{ hours} =\\) \\( 24 \\times 60 \\text{ minutes} =\\) \\( 1440 \\text{ minutes}\\).",
+    "Unless stated otherwise, the data in each function is related to the task occurring at each particular minute.",
+    "Based on the discussion in <i>Rationale for choice of Metrics and Parameters</i>.",
+    "It should be noted that plots for real users are almost never continuous, unlike the plot in this example."
 ];
 
 const cites = $("sup[data-cite]");
@@ -15,3 +18,9 @@ for (var x = 0; x < cites.length; x++) {
     $(cites[x]).text(x + 1);
     $("#citations").append("<li>" + citations[$(cites[x]).attr('data-cite')] + "</li>");
 }
+
+window.MathJax = {
+    tex: {
+      inlineMath: [['$', '$'], ['\\(', '\\)']]
+    }
+  };
